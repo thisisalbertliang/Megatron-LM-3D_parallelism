836bb27c055c6c8eeb51470f26e5267a4e35edd6
sangkeun00 <sangkeun00@gmail.com>
2021-03-17 00:06:15 -0400

    delete phoebe specific files

--- kubernetes/docker.sh
+++
@@ -1,9 +0,0 @@
-DIR=$(dirname $0)
-
-#docker system prune
-
-# Docker build
-#docker build -t ajliang/dse-experiments .
-docker build -t ajliang/dse-experiments -f $DIR/Dockerfile $DIR/..
-
-docker push ajliang/dse-experiments
